cc7ee967fa9dacbe4685838e16c68b134f16afa0
Rakatan2024 <rakatan228322@gmail.com>
2023-09-14 11:21:24 +0600

    homework1+testing

--- homeworks/homework1/compareSlices.go
+++ homeworks/homework1/compareSlices.go
@@ -1,31 +1,19 @@
 package hw1
 
-func sort(slice1 []int) []int {
-	for i := 0; i < len(slice1)-1; i++ {
-		swapped := false
-		for j := 0; j < len(slice1)-1-i; j++ {
-			if slice1[j] > slice1[j+1] {
-				slice1[j], slice1[j+1] = slice1[j+1], slice1[j]
-				swapped = true
-			}
-		}
-		if !swapped {
-			break
-		}
-	}
-	return slice1
-}
-
 func Compare(slice1, slice2 []int) bool {
-	slice1 = sort(slice1)
-	slice2 = sort(slice2)
+	map1 := make(map[int]int,len(slice1))
+	map2 := make(map[int]int,len(slice2))
 	if len(slice1) != len(slice2) {
 		return false
 	}
 	for i := 0; i < len(slice1); i++ {
-		if slice1[i] != slice2[i] {
-			return false
-		}
+		map1[slice1[i]]++
+		map2[slice2[i]]++
 	}
+	for key,val := range map1 {
+		if val!= map2[key] {
+            return false
+        }
+    }
 	return true
 }
